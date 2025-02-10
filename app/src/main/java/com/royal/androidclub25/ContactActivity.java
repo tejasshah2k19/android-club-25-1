1da4c0138e0518460ef6a7215a65c78015238a83
Tejas Shah <tejasshah2k19@gmail.com>
2025-02-10 14:09:36 +0530

    image button click

--- app/src/main/java/com/royal/androidclub25/ContactActivity.java
+++ app/src/main/java/com/royal/androidclub25/ContactActivity.java
@@ -1,14 +1,26 @@
 package com.royal.androidclub25;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ContactActivity extends AppCompatActivity {
+
+
+    ImageButton btnCall;
+    ImageButton btnSearch;
+    ImageButton btnUser;
+
+    ImageView imgMaster;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,54 @@ public class ContactActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //bind
+        btnCall = findViewById(R.id.imgBtnContactAdd);
+        btnSearch = findViewById(R.id.imgBtnContactSearch);
+        btnUser = findViewById(R.id.imgBtnContactUser);
+        imgMaster = findViewById(R.id.imgViewHolder);
+
+
+        btnUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeUser();
+            }
+        });
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }//onCreate
+
+    //private
+    void changeCall(){
+
     }
-}
+
+    void changeSearch(){
+
+    }
+
+    void changeUser()
+    {
+        Drawable d = ContextCompat.getDrawable(getApplicationContext(),R.drawable.user_24);
+        btnUser.setImageDrawable(d);
+
+
+
+
+    }
+
+}//class
